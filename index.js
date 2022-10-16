19a4a8d5954bafb2ee34368ceb601bb3cc52203c
Karthikeya Enge <karthikeya.enge@hotmail.com>
2022-10-16 19:02:10 +0530

    updated index

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 document.querySelector('#n').addEventListener("click",(event)=>{
     let p=document.querySelector('#n');
+    let a=document.querySelector("#switch-btn");
 
     if(p.checked==1){
-        document.querySelector('#switch-btn').style.backgroundColor="crimson";
+        a.style.backgroundColor="crimson";
+        a.style.boxShadow="0px 0px 20px var(--BG-COLOR) inset,0px 0px 20px var(--ALT-COLOR2)"
+        document.querySelector('#barren').style.display="block";
+        document.querySelector('#verdant').style.display="none";
+
     }
 
     if(p.checked==0){
-        document.querySelector('#switch-btn').style.backgroundColor="green";
+        a.style.backgroundColor="green";
+        a.style.boxShadow="0px 0px 20px var(--BG-COLOR) inset,0px 0px 20px var(--ALT-COLOR)"
+        document.querySelector('#verdant').style.display="block";
+        document.querySelector('#barren').style.display="none";
     }
 })
